be05b355f551168624a2a3fb61e14151af7ee9f6
Isabella B <bellabrambilla@gmail.com>
2022-10-10 16:23:11 -0300

    template string perfil

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -2,31 +2,34 @@
 import {signUp} from "../../lib/services.js";
 
 export default () => {
-  let containerRegister = document.createElement("div");
-
+  let containerRegister = document.createElement("div");;
+  containerLogin.className= "desktopTelaCadastro"
   const register = `<header>mia.www</header>
-
-  <main>
-    <button class="btn" id="btn-entrar-Google">Entrar com Google</button>
-      <form id="form" class="register">
-        <div class="emailRegister">
-          <label for="e-mail">E-mail</label>
-          <input type="text" class="input" id="emailRegister">
-        </div>
-        <div class="nameRegister">
-          <label for="nome">Nome completo</label>
-          <input type="text" class="input" id="nameRegister">
-        </div>
-        <div class="usernameRegister">
-          <label for="nome de usuario">Nome de usuário</label>
-          <input type="text" class="input" id="usernameRegister">
-        </div>
-        <div class="passwordRegister">
-          <label for="senha">Senha</label>
-          <input type="text" class="input" id="passwordRegister">
-        </div>
-          <button class="btn" id="btnRegister">Cadastre-se</button>
-      </form>
+    <figure><img logo></figure>
+    <text>Lorenbbdsvj</text>
+    <figure><img novelo></figure>
+    <div class="telaCadastro">
+      <button class="btn" id="btn-entrar-Google">Entrar com Google</button>
+        <form id="form" class="register">
+          <div class="emailRegister">
+            <label for="e-mail">E-mail</label>
+            <input type="text" class="input" id="emailRegister">
+          </div>
+          <div class="nameRegister">
+            <label for="nome">Nome completo</label>
+            <input type="text" class="input" id="nameRegister">
+          </div>
+          <div class="usernameRegister">
+            <label for="nome de usuario">Nome de usuário</label>
+            <input type="text" class="input" id="usernameRegister">
+          </div>
+          <div class="passwordRegister">
+            <label for="senha">Senha</label>
+            <input type="text" class="input" id="passwordRegister">
+          </div>
+            <button class="btn" id="btnRegister">Cadastre-se</button>
+        </form>
+    </div>
   </main>
   `;
   containerRegister.innerHTML = register;
--- src/pages/criarperfil/index.js
+++ src/pages/criarperfil/index.js
@@ -2,9 +2,25 @@
 
 export default () => {
     let containerProfile= document.createElement("div")
-    
+    containerProfile.className= "telaProfile"
     let profile = `
     <header>Perfil</header>
+
+    <main> 
+      <div class="cover"></div>
+      <figure class="profile-pic"></figure>
+      <text class="name"></div>
+      <button class="edit"></button>
+      <button class="paw"></button>
+      <text class="bio"></text>
+      <section class="container-post">
+        <figure class="post-pic"></figure>
+        <h1>Nome do gato</h1>
+        <time datetime="2017-10-31T11:21:00+02:00">Terça, 31 Outubro 2017</time>
+        <link class="hash1"></link>
+        <article class="post"></article>
+      </section>
+    </main>
     `;
     containerProfile.innerHTML= profile
   
